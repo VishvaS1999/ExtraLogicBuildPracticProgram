7d8475fae17f589a799afe0f5aabd94c811f9b17
Vishva <gvishva2601@gmail.com>
2022-11-18 16:27:26 +0530

    Print Fibonacci Series Program

---
+++ src/com/bridgelabz/FibonacciSeries.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+// Fibonacci Series is 0 1 1 2 3 5 8 13....
+
+import java.util.Scanner;
+
+public class FibonacciSeries
+{
+    public static void main(String args[])
+    {
+        int a=0, b=1, c, number;
+
+        System.out.println("Enter Number:");
+
+        Scanner sc=new Scanner(System.in);
+        number=sc.nextInt();
+
+        for(int i=1; i<=number; i++)
+        {
+            System.out.println(a);
+            c=a+b;
+            a=b;
+            b=c;
+        }
+    }
+}
