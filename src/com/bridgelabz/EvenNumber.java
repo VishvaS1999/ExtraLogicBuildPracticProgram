a172b5ade9fb7b1544b211a140752b272bb69e53
Vishva <gvishva2601@gmail.com>
2022-11-18 23:37:42 +0530

    Print For 1 To 100 Even Numbers

---
+++ src/com/bridgelabz/EvenNumber.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class EvenNumber
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Even Number is:");
+
+        for(int i=2; i<=100 ; i++)
+        {
+            if(i % 2 == 0)
+
+                System.out.println(i);
+        }
+    }
+}
