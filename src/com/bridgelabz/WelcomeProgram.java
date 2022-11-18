0131f830fa06750fc29ad4816c19781e9f5dfaec
Vishva <gvishva2601@gmail.com>
2022-11-18 16:25:53 +0530

    Welcome To The Logic Build Program..

---
+++ src/com/bridgelabz/WelcomeProgram.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class WelcomeProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome Logic Build Program!...");
+
+    }
+}
